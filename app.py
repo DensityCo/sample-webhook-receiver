fce7f072c77c3b947413cc1a6c9eb872cfe0e4e4
Brian Weinreich <brian@density.io>
2019-05-02 14:20:50 -0400

    updated

--- app.py
+++ app.py
@@ -1,18 +1,12 @@
-from flask import Flask
+from flask import Flask, Response
 from datetime import datetime
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    return Response(status=200)
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+    app.run(debug=False, use_reloader=True)
